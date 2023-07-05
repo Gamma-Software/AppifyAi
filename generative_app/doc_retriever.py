be6b1bb1b6d8e990c5b8df11f31ada694e015575
Valenti Rudloff <valentin.rudloff.perso@gmail.com>
2023-07-05 10:23:19 +0200

    generate vectorstore db

--- generative_app/doc_retriever.py
+++ generative_app/doc_retriever.py
@@ -41,11 +41,11 @@ def num_tokens_from_string(docs: List[Document], encoding_name: str) -> int:
 
 def load_streamlit_docs() -> VectorStoreRetriever:
     # Check if the Chroma database exists
-    if not os.path.exists("document_database/streamlit_chroma_db"):
+    if not os.path.exists(".doc_db/streamlit_chroma_db"):
         raise Exception("The Chroma database for Streamlit does not exist. Please run the script `doc_retriever.py` to create it.")
 
     # load from disk
-    retriever = Chroma(persist_directory="document_database/streamlit_chroma_db").as_retriever()
+    retriever = Chroma(persist_directory=".doc_db/streamlit_chroma_db").as_retriever()
     retriever.search_kwargs["distance_metric"] = "cos"
     retriever.search_kwargs["fetch_k"] = 20
     retriever.search_kwargs["maximal_marginal_relevance"] = True
@@ -53,18 +53,18 @@ def load_streamlit_docs() -> VectorStoreRetriever:
     return retriever
 
 if __name__ == "__main__":
-    openai_api_key = sys.argv[1]
-    if openai_api_key is None:
+    if len(sys.argv) < 2:
         print("OpenAI API key is missing! Please add it in argument.")
         exit(1)
+    openai_api_key = sys.argv[1]
 
     # Check if the Chroma database exists
-    if os.path.exists("document_database/streamlit_chroma_db"):
+    if os.path.exists(".doc_db/streamlit_chroma_db"):
         try:
             choice = input("The Chroma database for Streamlit already exists. "
                         "Press Enter to delete it and create a new one. Press Ctrl+C or enter anything else to cancel.")
             if ["y", "Y", "yes", "Yes", "YES", ""] in choice:
-                shutil.rmtree("document_database/streamlit_chroma_db")
+                shutil.rmtree(".doc_db/streamlit_chroma_db")
             else:
                 raise KeyboardInterrupt
         except KeyboardInterrupt:
@@ -98,5 +98,9 @@ if __name__ == "__main__":
 
     print("=== Create embeddings and save them into Chroma Database for later use...")
     embeddings_model = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    vectorstore = Chroma.from_documents(documents=streamlit_source_code_doc,embedding=embeddings_model, persist_directory=f"document_database/streamlit_chroma_db")
+    vectorstore = Chroma.from_documents(documents=streamlit_source_code_doc,embedding=embeddings_model, persist_directory=f".doc_db/streamlit_chroma_db")
     vectorstore.persist()
+
+    print("=== Remove Streamlit codesource.")
+    if os.path.exists("streamlit"):
+        shutil.rmtree("streamlit")
