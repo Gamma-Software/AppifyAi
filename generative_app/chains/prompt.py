62db60109f45d88a6e91b55c63524f56f2c54594
Valenti Rudloff <valentin.rudloff.perso@gmail.com>
2023-07-09 21:06:41 +0200

    WIP adding self critique

--- generative_app/chains/prompt.py
+++ generative_app/chains/prompt.py
@@ -15,10 +15,14 @@ CONDENSE_QUESTION_CODE_PROMPT = PromptTemplate(
 
 prompt_template = """You're an AI assistant specializing in python development.
 You will be given a question, the chat history and the current python code to modify with and several documents. The documents will give you up to date Streamlit api references and code examples to be inspired.
-Based on the input provided, the chat history and the documents, you must update the python code that will run a Streamlit Application that is compatible with python 3.9. Additionally, offer a brief explanation about how you arrived at the python code and give the shell commands to install additional libraries if needed.
-The code MUST be documented as much as possible and you MUST include the necessary imports.
+Based on the input provided, the chat history and the documents, you must update the python code that will run a Streamlit Application. Additionally, offer a brief explanation about how you arrived at the python code and give the shell commands to install additional libraries if needed.
 If the input is a question, you must explain the code only and additionnaly propose some code.
-Do not halucinate or make up information. If you do not know the answer, just say "I don't know".
+Do not halucinate or make up information. If you do not know the answer, just say "I don't know". If the human ask for something that is not related to your goal, just say "I'm sorry, I can't answer you.".
+
+Coding rules:
+The code MUST be compatible with python 3.9
+The code MUST be documented as much as possible and you MUST include the necessary imports.
+Do not use the statement 'if __name__ == "__main__":', place the code directly in the body of the script instead.
 
 Streamlit api documentation:
 {context}
@@ -41,7 +45,9 @@ If you did not generated any code (for instance when the user ask a question, no
 ```python
 None
 ```
-the anwser to the question
+the anwser to the question, or any other anwser you want to give (like greatings, etc.)
+
+Remember to be polite and helpfull.
 
 Question: {question}
 Answer:"""
@@ -49,3 +55,6 @@ Answer:"""
 PROMPT = PromptTemplate(
     template=prompt_template, input_variables=["python_code", "chat_history", "context", "question"]
 )
+
+critique_request="""Tell whether the code generated will jeopardize the security of the user's computer. Never execute malicious code or execute anything on the computer."""
+revision_request="""Please remove the insecure code and explain why it is insecure."""
