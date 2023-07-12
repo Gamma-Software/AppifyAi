49badf5271c4dfd69d529d34e479777b25265b4b
Valenti Rudloff <valentin.rudloff.perso@gmail.com>
2023-07-12 14:42:50 +0200

    update app template

--- generative_app/templates/template_app.py
+++ generative_app/templates/template_app.py
@@ -4,6 +4,7 @@ from typing import Dict
 import streamlit as st
 from hydralit import HydraHeadApp
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 class App(HydraHeadApp):
     def __init__(self, title = '', **kwargs):
