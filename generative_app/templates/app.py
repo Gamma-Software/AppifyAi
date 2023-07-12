d5ca7c9bd050470256bf62fda428b81a8968e3c0
Valenti Rudloff <valentin.rudloff.perso@gmail.com>
2023-07-12 14:42:50 +0200

    update app template

--- generative_app/templates/app.py
+++ generative_app/templates/app.py
@@ -4,23 +4,14 @@ from typing import Dict
 import streamlit as st
 from hydralit import HydraHeadApp
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 class App(HydraHeadApp):
-    """
-    This is an example signup application to be used to secure access within a HydraApp streamlit application.
-
-    This application is an example of allowing an application to run from the login without requiring authentication.
-
-    """
-
     def __init__(self, title = '', **kwargs):
         self.__dict__.update(kwargs)
         self.title = title
 
 
     def run(self) -> None:
-        """
-        Application entry point.
-        """
         #---start
         #---end
